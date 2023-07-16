bb1a27c99c793a401553c5da6071ea6caaa3ddf2
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-07-16 13:28:00 -0300

    terminamos.

--- sqs.js
+++ sqs.js
@@ -1,7 +1,13 @@
 function getMessages() {
   fetch("http://localhost:3000/api/get-messages").then((response) =>
     response.json().then((json) => {
-      console.log(json);
+      const messsagesSection = document.getElementById("messages");
+      messsagesSection.innerHTML = "";
+      for (messageObj of json.Messages) {
+        const span = document.createElement("span");
+        span.innerText = messageObj.Body;
+        messsagesSection.appendChild(span);
+      }
     })
   );
 }
